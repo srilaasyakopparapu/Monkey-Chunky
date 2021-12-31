d7689dc3eb4946b70f1061c61a3902ee1cf2510f
srilaasyakopparapu <laasya.kopparapu@gmail.com>
2021-12-30 17:05:13 -0800

    Change the background color

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Header
-          backgroundColor={'#9c8210'}
+          backgroundColor={'red'}
           centerComponent={{
             text: 'Monkey Chunky',
             style: { color: '#fff', fontSize: 20 },
@@ -95,6 +95,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     padding: 10,
     margin: 10,
+    backgroundColor: 'yellow'
   },
   buttonText: {
     textAlign: 'center',
